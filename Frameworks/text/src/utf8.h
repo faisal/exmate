b33074114d1154931d62e1febd36ef64880d2f65
Faisal N Jawdat <faisal@faisal.com>
2026-02-25 14:38:26 +0000

    PR 11b: Updated to modern (and not deprecated) iterator declarations

--- Frameworks/text/src/utf8.h
+++ Frameworks/text/src/utf8.h
@@ -55,9 +55,16 @@ namespace utf8
 		return std::string(res, res + strLen);
 	}
 
+
 	template <typename _Iter>
-	struct iterator_t : public std::iterator<std::bidirectional_iterator_tag, uint32_t>
+	struct iterator_t
 	{
+		using iterator_category = std::bidirectional_iterator_tag;
+		using value_type = uint32_t;
+		using difference_type = std::ptrdiff_t;
+		using pointer = value_type*;
+		using reference = value_type&;
+
 		typedef iterator_t self;
 
 		iterator_t (_Iter const& base_iterator) : base_iterator(base_iterator) { }
@@ -238,8 +245,13 @@ namespace utf8
 namespace diacritics
 {
 	template <typename _Iter>
-	struct iterator_t : public std::iterator<std::bidirectional_iterator_tag, uint32_t>
+	struct iterator_t
 	{
+		using iterator_category = std::bidirectional_iterator_tag;
+		using value_type = uint32_t;
+		using difference_type = std::ptrdiff_t;
+		using pointer = value_type*;
+		using reference = value_type&;
 		typedef iterator_t self;
 
 		iterator_t (utf8::iterator_t<_Iter> const& first, utf8::iterator_t<_Iter> const& last) : current(first), stop(last) { }
